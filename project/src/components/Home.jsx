258b5bfa35fc2ae4bc37861c377fd247156d46ce
molina <molinamark444@gmail.com>
2024-10-15 20:23:39 +0800

    bago

--- project/src/components/Home.jsx
+++ project/src/components/Home.jsx
@@ -1,10 +1,9 @@
 // src/components/Home.js
 
-import React from 'react';
-import Navbar from './Navbar';
-import TourCard from './TourCard';
-import './Home.css'; // Ensure you create this CSS file for home styles
-
+import React from "react";
+import Navbar from "./Navbar";
+import TourCard from "./TourCard";
+import "./Home.css"; // Ensure you create this CSS file for home styles
 
 const Home = () => {
   const tours = [
@@ -14,7 +13,7 @@ const Home = () => {
       price: 99,
       startTime: "9:00 AM",
       duration: "3 hours",
-      image: "path/to/image1.jpg"
+      image: "path/to/image1.jpg",
     },
     {
       title: "Beach Getaway",
@@ -22,7 +21,7 @@ const Home = () => {
       price: 150,
       startTime: "10:00 AM",
       duration: "5 hours",
-      image: "path/to/image2.jpg"
+      image: "path/to/image2.jpg",
     },
     {
       title: "Mountain Adventure",
@@ -30,7 +29,7 @@ const Home = () => {
       price: 120,
       startTime: "8:00 AM",
       duration: "4 hours",
-      image: "path/to/image3.jpg"
+      image: "path/to/image3.jpg",
     },
   ];
 
@@ -41,7 +40,7 @@ const Home = () => {
       price: 200,
       startTime: "4:00 PM",
       duration: "6 hours",
-      image: "path/to/image4.jpg"
+      image: "path/to/image4.jpg",
     },
     {
       title: "Jungle Expedition",
@@ -49,7 +48,7 @@ const Home = () => {
       price: 180,
       startTime: "7:00 AM",
       duration: "8 hours",
-      image: "path/to/image5.jpg"
+      image: "path/to/image5.jpg",
     },
     {
       title: "Waterfall Hike",
@@ -57,7 +56,7 @@ const Home = () => {
       price: 130,
       startTime: "11:00 AM",
       duration: "4 hours",
-      image: "path/to/image6.jpg"
+      image: "path/to/image6.jpg",
     },
     {
       title: "City Night Tour",
@@ -65,7 +64,7 @@ const Home = () => {
       price: 120,
       startTime: "8:00 PM",
       duration: "3 hours",
-      image: "path/to/image7.jpg"
+      image: "path/to/image7.jpg",
     },
     {
       title: "Boat Cruise",
@@ -73,8 +72,8 @@ const Home = () => {
       price: 250,
       startTime: "12:00 PM",
       duration: "5 hours",
-      image: "path/to/image8.jpg"
-    }
+      image: "path/to/image8.jpg",
+    },
   ];
 
   return (
@@ -87,40 +86,37 @@ const Home = () => {
         ))}
       </div>
 
-     <div className="bodycard">
-<h2>Top 5 Tourists Destinations</h2>
-<div className="bodycard1">
-<div className="bodycard2">
-<label>El Nido, Palawan</label>
-<div className="img"/>
-<p>Paragraph lorem ipsum</p>
-</div>
-<div className="bodycard2">
-<label>El Nido, Palawan</label>
-<div className="img"/>
-<p>Paragraph lorem ipsum</p>
-</div>
-<div className="bodycard2">
-<label>El Nido, Palawan</label>
-<div className="img"/>
-<p>Paragraph lorem ipsum</p>
-</div>
-<div className="bodycard2">
-<label>El Nido, Palawan</label>
-<div className="img"/>
-<p>Paragraph lorem ipsum</p>
-</div>
-<div className="bodycard2">
-<label>El Nido, Palawan</label>
-<div className="img"/>
-<p>Paragraph lorem ipsum</p>
-</div>
-</div>
-</div>
-
-</div>
-
-
+      <div className="bodycard">
+        <h2>Top 5 Tourists Destinations</h2>
+        <div className="bodycard1">
+          <div className="bodycard2">
+            <label>El Nido, Palawan</label>
+            <div className="img" />
+            <p>Paragraph lorem ipsum</p>
+          </div>
+          <div className="bodycard2">
+            <label>El Nido, Palawan</label>
+            <div className="img" />
+            <p>Paragraph lorem ipsum</p>
+          </div>
+          <div className="bodycard2">
+            <label>El Nido, Palawan</label>
+            <div className="img" />
+            <p>Paragraph lorem ipsum</p>
+          </div>
+          <div className="bodycard2">
+            <label>El Nido, Palawan</label>
+            <div className="img" />
+            <p>Paragraph lorem ipsum</p>
+          </div>
+          <div className="bodycard2">
+            <label>El Nido, Palawan</label>
+            <div className="img" />
+            <p>Paragraph lorem ipsum</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
